412e3d61068ccba7f83bafbf822bdca3ff00c127
EricaReis <ericareis67@outlook.com>
2022-10-29 11:50:51 -0300

    Principio SOLID ISP

---
+++ isp/src/index.ts
@@ -0,0 +1,5 @@
+import Car from "./vehicles/Car";
+import MotorCycle from "./vehicles/Motorcycle";
+
+const car = new Car("Azul", 2003, 1.0, 4);
+const motorcycle = new MotorCycle("Branca", 2022, 324);
