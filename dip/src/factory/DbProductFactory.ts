5f0ae050a1f55196836804440d220c712a04045c
EricaReis <ericareis67@outlook.com>
2022-10-29 14:14:55 -0300

    Principio SOLID DIP

---
+++ dip/src/factory/DbProductFactory.ts
@@ -0,0 +1,14 @@
+import Db from "../model/Db";
+import IDbProduct from "../model/IDbProduct";
+import MongoDBProduct from "../model/MongoDBProduct";
+import MySQLProduct from "../model/MySQLProduct";
+
+export default class DbProductFactory {
+  public static create(type: Db): IDbProduct {
+    if (type === Db.MYSQL) {
+      return new MySQLProduct();
+    } else {
+      return new MongoDBProduct();
+    }
+  }
+}
---
+++ dip/src/model/MySQLProduct.ts
@@ -0,0 +1,7 @@
+import IDbProduct from "./IDbProduct";
+
+export default class MySQLProduct implements IDbProduct {
+  getProductById(productID: string): string {
+    return `MySQL: Exibindo dados do produto ${productID}`;
+  }
+}
